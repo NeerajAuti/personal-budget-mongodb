e37ef8576f2749f71f1f2320786abc01bd0bad17
neeraj <nauti@uncc.com>
2020-10-08 22:15:05 -0400

    ''

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express');
-const json = require('./budget-data.json')
+const cors = require('cors');
+//const json = require('./budget-data.json')
 const app = express();
 const port = 3000;
 
-app.use('/',express.static('public'));
+app.use(cors());
 
-app.get('/hello',(req,res) => {
-    res.send('Hello World');
-});
-
+const json = {
+    "myBudget": [
+        {
+            "title": "Eat out",
+            "budget": 25
+        },
+        {
+            "title": "Utilities",
+            "budget": 45
+        },
+        {
+            "title": "Rent",
+            "budget": 110
+        },
+        {
+            "title": "Grocery",
+            "budget": 100
+        },
+        {
+            "title": "Mobile",
+            "budget": 27
+        },
+        {
+            "title": "Internet",
+            "budget": 10
+        },
+        {
+            "title": "University",
+            "budget": 150
+        },
+        {
+            "title": "Netflix",
+            "budget": 50
+        }
+    ]
+}
 app.get('/budget', (req, res) => {
     res.json(json);
 });
